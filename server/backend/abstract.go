4de2f11069a6f5f9024d04cb9446fdfc11ea2a9c
robinus2 <robin+github@us2.nl>
2019-04-16 22:58:38 +0200

    todo

--- server/backend/abstract.go
+++ server/backend/abstract.go
@@ -2,6 +2,7 @@ package backend
 
 type AbstractBackend interface {
 	Type() TypeBackend
+	// @todo properties object
 	Write(namespace int, series uint64, timestamp uint64, value float64) error
 }
 
