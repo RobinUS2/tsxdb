02af3b52eb876046aa66b27b31b0fcbdb174c740
Robin <robin+github@us2.nl>
2019-05-13 13:37:46 +0200

    expected

--- tools/hmac_test.go
+++ tools/hmac_test.go
@@ -1,6 +1,7 @@
 package tools_test
 
 import (
+	"fmt"
 	"github.com/RobinUS2/tsxdb/tools"
 	"testing"
 )
@@ -15,11 +16,17 @@ func TestHmac(t *testing.T) {
 	if signature == nil || len(signature) < 1 {
 		t.Error()
 	}
+	const expected = "5cc3c04381b3100c65a20540f17c1917676c5813a4ce0a7d0281f69e56775035c746da69f531bb8b13c57172aeb7e9d25fb18cecd070369c99ec7e49f7034b69"
+	res := fmt.Sprintf("%x", signature)
+	if res != expected {
+		t.Error(res, expected)
+	}
 }
+
 func TestHmacInt(t *testing.T) {
 	var secret = []byte("test")
 	signature := tools.HmacInt(secret, 1234)
-	if signature == 0 {
-		t.Error()
+	if signature != 4017713220 {
+		t.Error(signature)
 	}
 }
