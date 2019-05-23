fb9c9775f4f64a87dcb037484e36c82ff41fb571
Robin <robin+github@us2.nl>
2019-05-23 11:02:09 +0200

    travis docker

--- server/runner/docker_logs.sh
+++ server/runner/docker_logs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
-docker logs -f `docker ps | grep 'tsxdb-server' | awk '{print $1}'`
+LOGS_OPTS=${LOGS_OPTS:-"-f"}
+docker logs ${LOGS_OPTS} `docker ps | grep 'tsxdb-server' | awk '{print $1}'`
