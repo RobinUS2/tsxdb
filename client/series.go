fed5ebb3dd1e4d1ac3ebd09e25fdf82339847afc
Robin Verlangen <robin+github@us2.nl>
2020-08-21 13:34:40 +0200

    fix race, deprecate NewSeries

--- client/series.go
+++ client/series.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
 type Series struct {
@@ -13,6 +14,10 @@ type Series struct {
 	name      string
 
 	initMux sync.Mutex
+}
+
+func (series *Series) Id() uint64 {
+	return atomic.LoadUint64(&series.id)
 }
 
 func (series *Series) Name() string {
@@ -33,10 +38,30 @@ func (series *Series) Tags() []string {
 
 func (client *Instance) Series(name string, opts ...SeriesOpt) *Series {
 	s := NewSeries(name, client)
+	isNew := s.Id() < 1
+	if !isNew {
+		return s
+	}
+
+	// initialise
 	s.applyOpts(opts)
+
+	// eager init?
+	if client.opts.EagerInitSeries {
+		// async to not block it, errors are ignored, since this is just a best effort, will be done (and error-ed) in write anyway later if retried
+		go func() {
+			conn, err := client.GetConnection()
+			if err != nil {
+				return
+			}
+			_, _ = s.Init(conn)
+		}()
+	}
+
 	return s
 }
 
+// Deprecated: use *Instance.Series() instead
 func NewSeries(name string, client *Instance) *Series {
 	// automatic singleton to prevent connection re-init every time if user does not
 	if series := client.seriesPool.Get(name); series != nil {
@@ -48,18 +73,6 @@ func NewSeries(name string, client *Instance) *Series {
 		name:   name,
 		client: client,
 		id:     0, // will be populated on first usage
-	}
-
-	// eager init?
-	if client.opts.EagerInitSeries {
-		// async to not block it, errors are ignored, since this is just a best effort, will be done (and error-ed) in write anyway later if retried
-		go func() {
-			conn, err := client.GetConnection()
-			if err != nil {
-				return
-			}
-			_, _ = series.Init(conn)
-		}()
 	}
 
 	// set in pool
