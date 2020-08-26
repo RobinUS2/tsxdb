3458dad9ce1f627e2c28c80799e701016b5b1ed3
Robin Verlangen <robin+github@us2.nl>
2020-08-26 10:51:50 +0200

    extra rpc options

--- rpc/opts_connection.go
+++ rpc/opts_connection.go
@@ -1,17 +1,24 @@
 package rpc
 
+import "time"
+
 const DefaultListenPort = 1234
 const DefaultListenHost = "127.0.0.1"
+const DefaultConnectTimeout = 10 * time.Second // setup a new connection to the server
 
 type OptsConnection struct {
-	ListenPort int    `yaml:"listen_port"`
-	ListenHost string `yaml:"listen_host"`
-	AuthToken  string `yaml:"auth_token"`
+	ListenPort     int           `yaml:"listen_port"`
+	ListenHost     string        `yaml:"listen_host"`
+	AuthToken      string        `yaml:"auth_token"`
+	ConnectTimeout time.Duration `yaml:"connection_timeout"`
+	Debug          bool          `yaml:"debug"`
 }
 
 func NewOptsConnection() OptsConnection {
 	return OptsConnection{
-		ListenPort: DefaultListenPort,
-		ListenHost: DefaultListenHost,
+		ListenPort:     DefaultListenPort,
+		ListenHost:     DefaultListenHost,
+		ConnectTimeout: DefaultConnectTimeout,
+		Debug:          false,
 	}
 }
